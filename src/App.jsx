bd4959feb1545ff21ac80e615a547c24d3ecfe2f
Lucycat0511 <zachary.m.maynor@gmail.com>
2023-09-12 18:04:47 -0400

    create header

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,54 @@
 import { Outlet } from "react-router-dom";
 
-function App({ cartCount, promotion }) {
+function App({ cartCount }) {
   return (
-    <>
+    <div className="flex flex-col gap-8">
       <header>
         <div>
-          <h1>Plantify</h1>
-          <p>Search</p>
-          <i>Account</i>
+          <div className="flex mx-4 md:mx-12 py-2 items-center">
+            <h1 className="flex-1 text-xl md:text-2xl">Plantify</h1>
+            <div className="flex gap-4 md:gap-12 text-sm">
+              <button className="flex-col hidden lg:flex">
+                <span className="font-medium">Store Locator</span>
+                <span className="text-gray-400 text-xs italic">
+                  My Plant Shop
+                </span>
+              </button>
+
+              <button className="lg:hidden">Search</button>
+              <button className="flex-col hidden lg:flex">
+                <span className="font-medium">Find Product</span>
+                <span className="text-gray-400 text-xs italic">
+                  Quick Search
+                </span>
+              </button>
+
+              <button className="md:hidden">Account</button>
+              <button className="flex-col hidden md:flex">
+                <span className="font-medium">Hi! Sign In</span>
+                <span className="text-gray-400 text-xs italic">
+                  Your Account
+                </span>
+              </button>
+
+              <button>
+                <i className="fa-solid fa-basket-shopping fa-lg"></i>
+                <span className="text-xs">{`(${0})`}</span>
+              </button>
+            </div>
+          </div>
+          <div className="flex">
+            <button className="flex-1 border">Shop</button>
+            <button className="flex-1 border">Resources</button>
+          </div>
+        </div>
+        <div className="hidden">
+          <button>Shop</button>
+          <button>Resources</button>
           <i>
             Shopping Cart<span>{`(${cartCount})`}</span>
           </i>
         </div>
-        <div>
-          <button>Shop</button>
-          <button>Resources</button>
-        </div>
-        {promotion ? <h3>{promotion}</h3> : null}
       </header>
 
       <main>
@@ -32,7 +64,7 @@ function App({ cartCount, promotion }) {
         </div>
         <div>copyright</div>
       </footer>
-    </>
+    </div>
   );
 }
 
--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./routes/Home.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
